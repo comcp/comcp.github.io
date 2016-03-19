4c86a8c61b027e0693b8370eef35c7df2213911c
wx <comcp@126.com>
2016-03-19 19:31:42 +0800

    no message

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,18 @@
-console.log('This would be the main JS file.');
+/**
+*
+*
+**/
+	function XTools(){
+		this.creatTime=new Date();
+		return this;
+	}
+ 	function XPage(title,body){
+		this.title=title;
+		this.body=body;
+		return this;
+	}
+	window.onload=function(){
+	
+	window.tools=new XTools();
+	
+	}
